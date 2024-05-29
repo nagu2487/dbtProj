5ae6d9b094f51d33f62d024883340d51561be6d5
nagu2487 <nagubalakrish@gmail.com>
2024-05-30 09:54:20 +1000

    First checkin

---
+++ models/example/esg_report.sql
@@ -0,0 +1,21 @@
+WITH esg_report AS (SELECT
+    c.CompanyName,
+    c.Industry,
+    c.Headquarters,
+    em.Year,
+    em.CarbonEmissions,
+    em.EnergyConsumption,
+    em.WasteGenerated,
+    sgm.EmployeeTurnover,
+    sgm.DiversityScore,
+    sgm.BoardIndependence
+FROM
+    Companies c
+LEFT JOIN
+    EnvironmentalMetrics em ON c.CompanyID = em.CompanyID
+LEFT JOIN
+    SocialGovernanceMetrics sgm ON c.CompanyID = sgm.CompanyID AND em.Year = sgm.YEAR)
+    
+SELECT companyname, industry, headquarters, YEAR, carbonemissions, 
+ energyconsumption,wastegenerated, employeeturnover, diversityscore,boardindependence 
+ FROM esg_report
